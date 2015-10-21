c7470b5fdc229093526c9a59db4c71c7b11cdd04
Clement SORANO <clement.sorano@etudiant.univ-lille1.fr>
2015-10-21 09:52:31 +0200

    First Commit

---
+++ Android/src/main/java/org/Android/App.java
@@ -0,0 +1,13 @@
+package org.Android;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
