5cc50b0de31f89e30e102b53135d4aa2a70f87b8
Maximilien BONNARD <maximilien.bonnard@etud.univ-lille1.fr>
2015-10-21 14:37:47 +0200

    pour marcal android RDV

--- Android/App/BaP/app/src/main/java/bap/uit/com/bap/RDV.java
+++ Android/App/BaP/app/src/main/java/bap/uit/com/bap/RDV.java
@@ -1,12 +1,28 @@
 package bap.uit.com.bap;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+import static android.widget.Toast.*;
 
 public class RDV extends AppCompatActivity {
 
+
+    DialogFragment dial;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,5 +49,40 @@ public class RDV extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+/*
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+        }
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+*/
+
+
+    public void doValide(View v){
+        makeText(getApplicationContext(),"Worth it",LENGTH_LONG).show();
+        Intent intent = new Intent(RDV.this,Jesuis.class);
+        startActivity(intent);
     }
 }
