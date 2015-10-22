335d8fcf5392a91742bff640760b56738be97651
Theo ANGE <theo.ange@etudiant.univ-lille1.fr>
2015-10-22 15:30:59 +0200

    ClientResource

---
+++ Serveur/src/main/java/org/Serveur/ClientResource.java
@@ -0,0 +1,35 @@
+package org.Serveur;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+
+
+
+@Path("/client")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ClientResource {
+
+	@POST
+	public Client createClient(Client client) {
+		int id = Init.getInstance().getClientDao().insert(client);
+		client.setIdt(id);
+		return client;
+	}
+	
+	@GET
+	@Path("/{id}")
+	public Client getClient(@PathParam("id") int idt) {
+		Client client = Init.getInstance().getClientDao().findByIdt(idt);
+		if (client == null) {
+			throw new WebApplicationException(404);
+		}
+		return client;
+	}
+}
---
+++ Serveur/src/test/java/org/Serveur/ClientTest.java
@@ -0,0 +1,60 @@
+package org.Serveur;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClientTest extends JerseyTest {
+
+	@Override
+	protected Application configure() {
+		return new Api();
+	}
+	
+	@Before
+	public void init() {
+		Init.getInstance().initTables();
+	}
+	
+	@Test
+	public void should_return_an_id_for_new_client(){
+		Client client = new Client();
+		client.setNom("foo");
+		Entity<Client> clientEntity = Entity.entity(client,
+				MediaType.APPLICATION_JSON);
+		Client savedClient = target("/client").request().post(clientEntity)
+				.readEntity(Client.class);
+		assertEquals(1, savedClient.getIdt());
+	}
+	
+	@Test
+	public void should_return_good_client() {
+		Client client = new Client();
+		client.setNom("foobar");
+		Entity<Client> clientEntity = Entity.entity(client,
+				MediaType.APPLICATION_JSON);
+		Client savedClient = target("/client").request().post(clientEntity)
+				.readEntity(Client.class);
+
+		Client readClient = target("/client/" + savedClient.getIdt()).request()
+				.get(Client.class);
+		assertEquals("foobar", readClient.getNom());
+	}
+	
+
+	@Test(expected=WebApplicationException.class)
+	public void should_return_404_when_client_does_not_exist() {
+		
+		Client readClient = target("/client/120").request()
+				.get(Client.class);
+		assertEquals("foobar", readClient.getNom());
+	}
+	
+}
